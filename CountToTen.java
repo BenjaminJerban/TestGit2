dd1453941b495fa33ba431bb4df78623ae234570
Daniel Jarban <Daniel Jerban@DESKTOP-7H8MPBN>
2025-10-29 13:02:33 +0330

    comments added

--- CountToTen.java
+++ CountToTen.java
@@ -2,5 +2,6 @@ public class CountToTen {
     void countToTen(int tmep){
         for (int temp=0 ; temp<10 ; tmep++)
         System.out.println(temp);
+        // Count from zero to nine
     }
 }
