dc1233ee3c9b6517cd65609fbbf9fe98aed87e71
Daniel Jarban <Daniel Jerban@DESKTOP-7H8MPBN>
2025-10-29 13:58:41 +0330

    CountOdd class created

---
+++ CountOdd.java
@@ -0,0 +1,10 @@
+public class CountOdd {
+    // Counting the odd numbers lower than 10
+    void countOdd(int temp){
+        for (temp=0; temp<10; temp+=2)
+            System.out.println(temp+1);
+        
+    }
+
+    
+}
